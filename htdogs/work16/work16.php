4fb4cfa4b93ae5d00ba763699d420d60ae305f72
itex-omiya-konishi <it.omiya237@gmail.com>
2025-09-03 13:20:38 +0900

    a

---
+++ htdogs/work16/work16.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>WORK16</title>
+</head>
+
+<body>
+    <div>入力内容の取得</div>
+    <form method="get" action="work16_02.php">
+        <input type="text" name="name_text">
+
+        <label><input type="checkbox" name="check_b" value="option1"> 選択肢1</label><br>
+        <label><input type="checkbox" name="check_b" value="option2"> 選択肢2</label><br>
+        <label><input type="checkbox" name="check_b" value="option3"> 選択肢3</label><br>
+
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
---
+++ htdogs/work16/work16_02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>WORK16</title>
+</head>
+
+<body>
+    <div>フォームに入力した内容を取得する</div>
+    <?php
+    if (isset($_GET['name_text'])) {
+        print '入力した内容： ' . htmlspecialchars($_GET['name_text'], ENT_QUOTES, 'UTF-8');
+    } else {
+        print '入力されていません';
+    }
+    if (isset($_GET["check_b"])) {
+        print '選んだ選択肢： ' . htmlspecialchars($_GET['name_text'], ENT_QUOTES, 'UTF-8');
+    }
+    ?>
+</body>
+
+</html>
